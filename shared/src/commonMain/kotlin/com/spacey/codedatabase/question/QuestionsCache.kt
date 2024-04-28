c8c193e907853a27cbb6eb90c5d99090a52f9fc2
Subikesh <subikeshps@gmail.com>
2024-04-28 17:19:43 +0530

    Questions cache and filter logic implemented

---
+++ shared/src/commonMain/kotlin/com/spacey/codedatabase/question/QuestionsCache.kt
@@ -0,0 +1,5 @@
+package com.spacey.codedatabase.question
+
+class QuestionsCache {
+    var questions: List<Question>? = emptyList()
+}
--- shared/src/commonMain/kotlin/com/spacey/codedatabase/question/QuestionsRepository.kt
+++ shared/src/commonMain/kotlin/com/spacey/codedatabase/question/QuestionsRepository.kt
@@ -3,10 +3,29 @@ package com.spacey.codedatabase.question
 import com.spacey.codedatabase.BaseRepository
 import kotlinx.coroutines.withContext
 
-class QuestionsRepository(private val apiService: QuestionsApiService) : BaseRepository() {
+class QuestionsRepository(private val apiService: QuestionsApiService, private val questionsCache: QuestionsCache) : BaseRepository() {
     suspend fun getQuestions(): Result<List<Question>> {
         return withContext(defaultContext) {
-            apiService.getAllQuestions()
+            apiService.getAllQuestions().also {
+                questionsCache.questions = it.getOrNull()
+            }
         }
     }
+
+    /**
+     * Filter questions from the stored cache. If cache is empty, refresh the list
+     */
+    suspend fun filterQuestions(query: String): Result<List<Question>> {
+        val cache = questionsCache.questions
+        if (!cache.isNullOrEmpty()) {
+            return Result.success(if (query.isNotBlank()) cache.search(query) else cache)
+        }
+        return getQuestions().map { it.search(query) }
+    }
+
+    private fun List<Question>.search(query: String): List<Question> =
+        this.filter { question ->
+            question.title.split("\\s+".toRegex()).any { it.startsWith(query, ignoreCase = true) }
+//                    || question.description.startsWith(query, ignoreCase = true)
+        }
 }
